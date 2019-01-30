541745d30db9b7ea789f9cbbed6cc1e38f6adfea
ShinJeKim <shinjehere@gmail.com>
2019-01-30 23:34:36 +0900

    20190130 complete

--- Baekjoon/src/level/java/B_2441.java
+++ Baekjoon/src/level/java/B_2441.java
@@ -2,19 +2,22 @@ package level.java;
 
 import java.util.Scanner;
 
-public class B_2440 {
+public class B_2441 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		for (int y = 0; y < N; y++) {
-			for (int x = 0; x < N - y; x++) {
-				System.out.print("*");
+		for (int y = N; y > 0; y--) {
+			for (int x = N; x > 0; x--) {
+				if (x <= y) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
 			}
 			System.out.println();
 		}
-
 	}
 
 }
