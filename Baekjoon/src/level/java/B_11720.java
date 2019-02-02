751b9d996ba92c954420ddfb3f78b0e47adc1f71
ShinJeKim <shinjehere@gmail.com>
2019-02-02 17:18:26 +0900

    20190202 complete

---
+++ Baekjoon/src/level/java/B_11720.java
@@ -0,0 +1,23 @@
+package level.java;
+
+import java.util.Scanner;
+
+public class B_11720 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1. 입력받은 숫자를 String으로 저장
+		 * 2. String split해서 int로 변환하여 배열에 넣기 
+		 * 3. for문으로 배열 합 구하기 
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int[] arr = new int[input.length()];
+		
+		for(int i=0; i<arr.length; i++) {
+			char str = input.charAt(i);
+		}
+	}
+
+}
---
+++ Baekjoon/src/level/java/B_1924.java
@@ -0,0 +1,51 @@
+package level.java;
+
+import java.util.Scanner;
+
+public class B_1924 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int sum = 0;
+		String answer = "";
+		
+		String[] day = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+		
+		if(x == 1) {
+			answer = day[y%7];
+		}else {
+			for(int i=2; i<=x; i++) {
+				if(i == 2) {
+					sum += 28;
+				}else if(i<8) {
+					if(i%2 == 1)
+						sum += 31;
+					else
+						sum += 30;
+				}else {
+					if(i%2 ==1)
+						sum +=30;
+					else
+						sum += 31;
+				}
+			}
+			System.out.println("error = "+((sum+y)%7-1));
+			//answer = day[(sum+y)%7-1];
+			
+			if(((sum+y)%7-1) == -1) {
+				answer = day[6];
+			}else {
+				answer = day[(sum+y)%7-1];
+			}
+			
+		}
+		
+		
+		System.out.println(answer);
+
+		
+	}
+
+}
